86bf9529d97e3407ede4bbbbf961fc1337b219bf
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2024-09-11 23:54:41 -0300

    fix 1

--- sale_attachment_button/models/account_payment_group.py
+++ sale_attachment_button/models/account_payment_group.py
@@ -13,11 +13,11 @@ class AccountPaymentGroup(models.Model):
 
         domain = [
             '|', '|', '|', '|',
-            (('res_model', '=', 'account.payment.group'), ('res_id', 'in', [self.id]))
-            (('res_model', '=', 'account.payment'),('res_id', 'in', [related_payment_ids]))
-            (('res_model', '=', 'account.move'),('res_id', 'in', [related_invoice_ids]))
-            (('res_model', '=', 'sale.order'),('res_id', 'in', [related_sale_order_ids]))
-            (('res_model', '=', 'stock.picking'),('res_id', 'in', [related_picking_ids]))
+            '&',('res_model', '=', 'account.payment.group'), ('res_id', 'in', [self.id]),
+            '&',('res_model', '=', 'account.payment'),('res_id', 'in', [related_payment_ids]),
+            '&',('res_model', '=', 'account.move'),('res_id', 'in', [related_invoice_ids]),
+            '&',('res_model', '=', 'sale.order'),('res_id', 'in', [related_sale_order_ids]),
+            '&',('res_model', '=', 'stock.picking'),('res_id', 'in', [related_picking_ids]),
         ]
 
         return {
