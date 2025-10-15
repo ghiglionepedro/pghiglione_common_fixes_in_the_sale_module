06318bcaf4cbdfc3e9fb67be8d29f075b4261a0c
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2025-10-15 00:08:29 -0300

    restric

--- pghiglione_restrict_inventory_set/models/stock_quant.py
+++ pghiglione_restrict_inventory_set/models/stock_quant.py
@@ -4,7 +4,6 @@ from odoo.exceptions import UserError
 class StockQuant(models.Model):
     _inherit = "stock.quant"
 
-    @api.model
     def action_apply_inventory(self):
         user = self.env.user
         if not user.has_group("restrict_inventory_set.group_can_apply_inventory"):
