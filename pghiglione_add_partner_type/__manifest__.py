207bc98fe14fe49c3821b04c75325b0fa97d471e
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2026-01-12 01:21:33 -0300

    commit

--- pghiglione_add_partner_type/__manifest__.py
+++ pghiglione_add_partner_type/__manifest__.py
@@ -1,30 +1,14 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Añade un campo de tipo de ártner al listado de sale.order",
-
-    'summary': """Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "Ghiglione Pedro Matias",
-    'website': "https://quimerasoftware.ar",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['sale'],
-
-    # always loaded
+    'name': 'Partner Origin Type',
+    'version': '16.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Adds customer origin type to partners and sale orders',
+    'author': 'Quimera Software',
+    'depends': ['sale', 'contacts'],
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/res_partner_view.xml',
+        'views/sale_order_view.xml',
     ],
+    'installable': True,
+    'application': False,
 }
