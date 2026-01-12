bdc45470759aa612be682a5192bd976931e1f8d4
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2026-01-12 01:55:23 -0300

    commit

--- pghiglione_account_invoice_force_number_ar/models/account_move.py
+++ pghiglione_account_invoice_force_number_ar/models/account_move.py
@@ -5,15 +5,27 @@ from odoo.exceptions import ValidationError
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.constrains("move_name")
+    @api.constrains("move_name", "journal_id")
     def _check_move_name_afip_format(self):
         for move in self:
-            if move.move_name:
-                if not re.match(r"^FA-[ABC]\s\d{5}-\d{8}$", move.move_name):
-                    raise ValidationError(_(
-                        "Formato inválido de número de factura.\n\n"
-                        "Debe ser exactamente:\n"
-                        "FA-A 00003-00001234\n"
-                        "FA-B 00003-00001234\n"
-                        "FA-C 00003-00001234"
-                    ))
+            if not move.move_name:
+                continue
+
+            journal = move.journal_id
+
+            # Solo validar diarios AFIP
+            if not journal.l10n_ar_afip_pos_id:
+                continue
+
+            # Solo ventas (no compras ni asientos)
+            if move.move_type not in ("out_invoice", "out_refund"):
+                continue
+
+            if not re.match(r"^FA-[ABC]\s\d{5}-\d{8}$", move.move_name):
+                raise ValidationError(_(
+                    "Formato inválido de número de factura.\n\n"
+                    "Para diarios AFIP debe ser:\n"
+                    "FA-A 00003-00001234\n"
+                    "FA-B 00003-00001234\n"
+                    "FA-C 00003-00001234"
+                ))
